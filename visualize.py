6c0d6bce9847491da330ea4813c8bc7cf3e7a799
Mikołaj Badyl <contact@hawier.dev>
2022-11-04 13:38:28 +0100

    Top argument

--- visualize.py
+++ visualize.py
@@ -1,4 +1,3 @@
-import os
 from os.path import basename
 
 import gpxpy
@@ -15,11 +14,12 @@ parser.add_argument(
     "-g", "--path_gpx", type=str, help="Path to .gpx file", required=True
 )
 parser.add_argument(
-    "-l", "--path_las", type=str, help="Path to .las file", required=True
+    "-l", "--path_las", type=str, help="Path to .las file", default=None
 )
 parser.add_argument(
     "-c", "--color", type=str, help="Color of the gpx track", default="255 0 0"
 )
+parser.add_argument("-t", "--top", type=int, help="Value to increase Z", default=0)
 args = parser.parse_args()
 
 
@@ -27,29 +27,32 @@ file_name_no_ext = basename(args.path_gpx).replace(
     "." + args.path_gpx.split(".")[-1], ""
 )
 
+all_points = []
+all_points_colors = []
+
 # Convert las to xyzrgb format
-las_file = laspy.read(args.path_las)
+if args.path_las:
+    las_file = laspy.read(args.path_las)
 
-gpx_color = [int(c) for c in args.color.split(" ")]
-
-points_las = np.dstack(
-    [
-        las_file.x,
-        las_file.y,
-        las_file.z,
-    ]
-)
-points_colors_las = np.dstack(
-    [
-        las_file.red / 65280,
-        las_file.green / 65280,
-        las_file.blue / 65280,
-    ]
-)
+    all_points = np.dstack(
+        [
+            las_file.x,
+            las_file.y,
+            las_file.z,
+        ]
+    )
+    all_points_colors = np.dstack(
+        [
+            las_file.red / 65280,
+            las_file.green / 65280,
+            las_file.blue / 65280,
+        ]
+    )
 
 # Reading gpx data
 gpx_file = open(args.path_gpx, "r")
 gpx = gpxpy.parse(gpx_file)
+gpx_color = [int(c) for c in args.color.split(" ")]
 
 points_gpx = [[]]
 points_colors_gpx = [[]]
@@ -60,16 +63,20 @@ for track in gpx.tracks:
             y, x, z = transformer.transform(
                 point.latitude, point.longitude, point.elevation
             )
-            points_gpx[0].append([x, y, z + 20])
+            points_gpx[0].append([x, y, z + args.top])
             points_colors_gpx[0].append(
                 [gpx_color[0] / 255, gpx_color[1] / 255, gpx_color[2] / 255]
             )
 
-points_las = np.column_stack((points_las, points_gpx))
-points_colors_las = np.column_stack((points_colors_las, points_colors_gpx))
+if args.path_las:
+    all_points = np.column_stack((all_points, points_gpx))
+    all_points_colors = np.column_stack((all_points_colors, points_colors_gpx))
+else:
+    all_points = points_gpx
+    all_points_colors = points_colors_gpx
 
 # Visualization
 pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(points_las[0])
-pcd.colors = o3d.utility.Vector3dVector(points_colors_las[0])
+pcd.points = o3d.utility.Vector3dVector(all_points[0])
+pcd.colors = o3d.utility.Vector3dVector(all_points_colors[0])
 o3d.visualization.draw_geometries([pcd])
